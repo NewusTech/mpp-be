ba8c0508da0253854cf6bbabd1fff069db3a2ac8
muttaqin <muttaqin0708@gmail.com>
2024-07-15 11:49:02 +0700

    upodate bisa ubah actiove online/offline

---
+++ models/manualbook.js
@@ -0,0 +1,17 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Manualbook extends Model {
+    static associate(models) {
+    }
+  }
+  Manualbook.init({
+    dokumen: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Manualbook',
+  });
+  return Manualbook;
+};
